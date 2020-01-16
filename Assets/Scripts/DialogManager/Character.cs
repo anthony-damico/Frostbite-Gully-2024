b6db55538b1c0efe9ab9238a44d1c1ec1202d00f
anthony-damico <52482785+anthony-damico@users.noreply.github.com>
2020-01-16 21:29:35 +1100

    Dialog v0.1
    Started to implement dialog System.
    Its hard. Need to learn how to cycle through arrays

---
+++ Assets/Scripts/DialogManager/Character.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Dialog Options/Character")]
+public class Character : ScriptableObject
+{
+    public string characterName;        //The characters name that will appear when dialog is active
+    public Sprite characterPortrait;    //The characters portrait that will appear when dialog is active
+
+}
---
+++ Assets/Scripts/DialogManager/Dialog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Line
+{
+    public Character character;
+
+    [TextArea(4, 5)]
+    public string text;
+}
+
+[CreateAssetMenu(fileName = "New Dialog", menuName = "Dialog Options/Dialog")]
+public class Dialog : ScriptableObject
+{
+    public Line[] lines;
+}
+
